f8bd8fea602befcdc46f994596b41071c7e3bad0
“eugabrieldesousa” <eugabrieldesousa@gmail.com>
2025-02-02 08:52:53 -0300

    initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+
+import entities.Person;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Person giovana = new Person("giovana", 20);
+
+        System.out.println(giovana);
+
+    }
+}
